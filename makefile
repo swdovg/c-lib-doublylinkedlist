f36b871591c2d83b4cb1a82ce561269861ace4d1
Sergey Dovgopolyy <serge.dovg@gmail.com>
2018-01-14 06:15:40 +0300

    fixed getNth

--- makefile
+++
@@ -1,8 +0,0 @@
-CC=gcc
-CFLAGS=-Wall -I./src -o
-
-test: 	test-main/main.c src/doublylinkedlist.c
-	$(CC) $(CFLAGS) test test-main/main.c src/doublylinkedlist.c
-clean:
-	rm -rf *.o
-#
--- src/doublylinkedlist.c
+++ src/doublylinkedlist.c
@@ -110,6 +110,10 @@ Node* getNth(DblLinkedList *list, size_t index) {
     Node *tmp = list->head;
     size_t i = 0;
 
+    if ( i > index) {
+	exit(EXIT_FAILURE);
+    }
+
     while (tmp && i < index) {
         tmp = tmp->next;
         i++;
